f7cc36d20ccf256150c356b4d46381dd55258399
avbhutani <avbhutani3@gmail.com>
2024-03-05 13:41:50 +0530

    Made some changes to the frontend files

---
+++ frontend/script3.js
@@ -0,0 +1,107 @@
+const username = document.getElementById('username')
+const password = document.getElementById('password')
+const addNewUser = document.getElementById('new-user-button')
+const existingUser = document.getElementById('login-button')
+const statusUpdate = document.getElementById('display-status')
+
+let list = [];
+var user,pass;
+
+// This will add a new user if the user doesn't exist.
+addNewUser.addEventListener('click',(event)=> {
+
+  // This will check that both the username and password fields are filled or not.
+  // checkInputValidity();
+  event.preventDefault();
+
+  let uvalue = username.value;
+  let pvalue = password.value;
+  // if the username is not valid,then return.
+  // if(!checkValidUsername(uvalue)) {
+  //   alert("Username should only contain lowercase letters.");
+  //   return;
+  // }
+  
+  // if the password is not valid then return.
+  // if(!checkValidPassword(pvalue)) {
+  //   alert(`Password should be atleast 8 chars long!`);
+  //   return;
+  // }
+
+  let tempUser = {
+    name1:uvalue,
+    password:pvalue
+  }
+  // Convert the data to a JSON string
+  fetch('http://localhost:3000/update', {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(tempUser)
+})
+   .then(response => response.json())
+   .then(response => {
+    if(response === false) {
+      alert("User already Exists");
+      return;
+    }
+    return console.log(JSON.stringify(response))})
+   
+  statusUpdate.textContent = 'User Added Successfully!';
+});
+
+// // This function will check if there's an existing username with the same username that is entered.
+// existingUser.addEventListener('click',(event)=> {
+//   checkInputValidity();
+//   event.preventDefault();
+//   let login = false;
+//   for(u in list) {
+//     if(list[u].name === username.value) {
+//       console.log(`${username.value} successfully logged in.`)
+//       statusUpdate.textContent = `${username.value} successfully logged in.`
+//       login = true;
+//     }
+//   }
+//   if(!login) {
+//     statusUpdate.textContent = `User doesn't exist. Kindly SignUp!`
+
+//   } 
+// })
+
+
+// // This will check whether the username and password fields are filled or not.
+// function checkInputValidity() {
+//   if(!username.value || (!username.value && password.value)) {
+//     alert(`Username can't be empty!`);
+//   }
+//   else if(username.value && !password.value) {
+//     alert(`Please Enter a Password!`);
+//   }
+//   else if(!username.value && !password.value) {
+//     alert(`Both fields cannot be empty!`);
+//   }
+// }
+
+
+// // This function will check whether the username is valid or not.
+// function checkValidUsername(uvalue) {
+//   for(let i = 0;i<uvalue.length;i++) {
+//     let aa = uvalue.charCodeAt(i);
+//     if( aa < 97 || aa > 122) {
+//       return false;
+//     }
+//   }
+//   return uvalue;
+// }
+
+// // This function will check whether the password is valid or not.
+// function checkValidPassword(pvalue) {
+//   if(pvalue.length < 8) {
+//     return 0;
+//   }
+//   return pvalue;
+// }
+
+
